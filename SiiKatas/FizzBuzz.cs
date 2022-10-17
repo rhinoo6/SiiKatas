d9d64736b6bb04a7ce8dad7140369b9e5808ae66
jinyan.yang <jinyan.yang@FRNAN-L00082>
2022-10-17 16:49:21 +0200

    partie 1

--- SiiKatas/FizzBuzz.cs
+++ SiiKatas/FizzBuzz.cs
@@ -7,6 +7,19 @@ namespace SiiKatas
         public string DoFizzBuzz(int number)
         {
             // Implement your code here
+            
+                if (number % 3 ==0)
+                {
+                    return "Fizz";
+                }
+                else if (number % 5 == 0)
+                {
+                    return "Buzz";
+                }
+                else
+                {
+                return null;
+                }
             throw new NotImplementedException();
         }
     }
