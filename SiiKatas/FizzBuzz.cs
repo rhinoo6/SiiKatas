3a8319ebf6fd5285fc92ffa5227cb225e64e5779
Romain <romain.bignon@external.desouttertools.com>
2022-10-17 12:02:16 +0200

    Ajoutez des fichiers projet.

---
+++ SiiKatas/FizzBuzz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SiiKatas
+{
+    public class FizzBuzz
+    {
+        public string DoFizzBuzz(int number)
+        {
+            // Implement your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SiiKatas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SiiKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzz();
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(fizzBuzz.DoFizzBuzz(i));
+            }
+        }
+    }
+}
